22e11508f1ab248ae02e7e1d9602fafd531ce4ba
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2021-12-23 17:19:35 +0900

    Add files via upload

---
+++ Licenser.php
@@ -0,0 +1,11 @@
+<?php
+if (array_key_exists("license", $_POST)) {
+    $license = $_POST["license"];
+    echo $license. PHP_EOL;
+    if ($license == "") {
+        $license = "none";
+    }
+    $file = fopen("license.txt", "w");
+    fwrite($file, $license);
+    fclose($file);
+}
